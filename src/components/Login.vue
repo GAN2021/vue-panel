cf275c5ed1a02dd50625ed5c5aa7eb96c1a7241f
GAN2021 <gan2021@com.com>
2021-09-12 18:31:40 +0800

    重置表单数据和验证提示

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -5,7 +5,7 @@
         <img src="../assets/logo.png" alt />
       </div>
       <!-- 登录表单 -->
-      <el-form :model="loginForm" :rules="loginFormRules" class="login_form">
+      <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" class="login_form">
         <!-- 用户名 -->
         <el-form-item prop="username">
           <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
@@ -21,7 +21,7 @@
         <!-- 按钮 -->
         <el-form-item class="btns">
           <el-button type="primary">登录</el-button>
-          <el-button type="info">重置</el-button>
+          <el-button type="info" @click="reset">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -48,6 +48,12 @@ export default {
           { min: 6, max: 15, message: '密码长度必须在 6 到 15 个字符之间', trigger: 'blur' }
         ]
       }
+    }
+  },
+  methods: {
+    // 重置表单和验证提示
+    reset: function () {
+      this.$refs.loginFormRef.resetFields()
     }
   }
 }
