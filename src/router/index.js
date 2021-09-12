bab5bd25f12a3f51f76560b2610fbe2914d0040d
GAN2021 <gan2021@com.com>
2021-09-12 21:22:58 +0800

    添加路由导航守卫，过滤未授权请求，导航到登录页

--- src/router/index.js
+++ src/router/index.js
@@ -13,4 +13,19 @@ const router = new VueRouter({
   ]
 })
 
+// 挂载路由导航守卫（就是个请求验证过滤器）
+router.beforeEach((to, from, next) => {
+  // 请求的是登录页，放行
+  if (to.path === '/login') {
+    next()
+  }
+  // 无效token，导航到登录页
+  const token = window.sessionStorage.getItem('token')
+  if (!token) {
+    next('/login')
+  }
+  // 有效token，放行
+  next()
+})
+
 export default router
