4b66e109027e8835d3178726cf892f5384a4de33
GAN2021 <gan2021@com.com>
2021-09-13 15:05:57 +0800

    主页布局和侧边栏菜单布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,42 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 导航栏区域 -->
+    <el-header>
+      <div>
+        <img src="../assets/logo.png" width="30px" height="30px" />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 中间区 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+          background-color="#2b3758"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-user"></i>
+              <span>用户列表</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-location"></i>
+              <span>选项一</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 主体区 -->
+      <el-main>主题区</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -16,4 +51,34 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.el-header {
+  background-color: #202941;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #eee;
+  font-size: 20px;
+  padding-left: 5px;
+
+  > div {
+    display: flex;
+    justify-content: space-between;
+
+    span {
+      padding-left: 15px;
+    }
+  }
+}
+.el-aside {
+  background-color: #2b3758;
+}
+.el-main {
+  background-color: #eee;
+}
+.home-container {
+  height: 100%;
+}
+.el-menu{
+  border-right: 0px;
+}
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -4,12 +4,29 @@ import {
   FormItem,
   Button,
   Input,
-  Message
+  Message,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Menu,
+  Submenu,
+  MenuItem,
+  MenuItemGroup
 } from 'element-ui'
 
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Button)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
 // 全局挂载
 Vue.prototype.$message = Message
