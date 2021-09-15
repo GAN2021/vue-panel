5744981c1af77f6ec76010991bfe1b9140cdd831
GAN2021 <gan2021@com.com>
2021-09-15 19:18:42 +0800

    [编辑用户]功能完成

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -42,14 +42,21 @@
           </template>
         </el-table-column>
         <el-table-column label="操作" width="180">
-          <!-- 编辑按钮 -->
-          <el-button size="mini" type="primary" icon="el-icon-edit"></el-button>
-          <!-- 删除按钮 -->
-          <el-button size="mini" type="danger" icon="el-icon-delete"></el-button>
-          <!-- 分配权限按钮 -->
-          <el-tooltip effect="dark" content="权限分配" placement="top" :enterable="false">
-            <el-button size="mini" type="warning" icon="el-icon-setting"></el-button>
-          </el-tooltip>
+          <template v-slot="scopeData">
+            <!-- 编辑按钮 -->
+            <el-button
+              size="mini"
+              @click="showEditDialog(scopeData.row.id)"
+              type="primary"
+              icon="el-icon-edit"
+            ></el-button>
+            <!-- 删除按钮 -->
+            <el-button size="mini" type="danger" icon="el-icon-delete"></el-button>
+            <!-- 分配权限按钮 -->
+            <el-tooltip effect="dark" content="权限分配" placement="top" :enterable="false">
+              <el-button size="mini" type="warning" icon="el-icon-setting"></el-button>
+            </el-tooltip>
+          </template>
         </el-table-column>
       </el-table>
 
@@ -85,6 +92,26 @@
       <span slot="footer" class="dialog-footer">
         <el-button type="primary" @click="addUser">提 交</el-button>
         <el-button @click="addDialogVisible = false">取 消</el-button>
+      </span>
+    </el-dialog>
+
+    <!-- 用户编辑对话框 -->
+    <el-dialog title="编辑用户" :visible.sync="editDialogVisible" @close="editDialogClose" width="50%">
+      <!-- 用户编辑表单 -->
+      <el-form :model="editForm" ref="editFormRef" :rules="editFormRules" label-width="70px">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="editForm.username" disabled></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="editForm.email"></el-input>
+        </el-form-item>
+        <el-form-item label="手机" prop="mobile">
+          <el-input v-model="editForm.mobile"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="updateUser">提 交</el-button>
+        <el-button @click="editDialogVisible = false">取 消</el-button>
       </span>
     </el-dialog>
   </div>
@@ -142,6 +169,21 @@ export default {
           { required: true, message: '请输入手机号', trigger: 'blur' },
           { validator: mobileValidator, message: '手机号格式不正确', trigger: 'blur' }
         ]
+      },
+      // [编辑用户]对话框可见性
+      editDialogVisible: false,
+      // [编辑用户]表单数据
+      editForm: {},
+      // [编辑用户]的验证规则
+      editFormRules: {
+        email: [
+          { required: true, message: '邮箱不能为空', trigger: 'blur' },
+          { validator: emailValidator, message: '邮箱格式不正确', trigger: 'blur' }
+        ],
+        mobile: [
+          { required: true, message: '手机号不能为空', trigger: 'blur' },
+          { validator: mobileValidator, message: '手机号格式不正确', trigger: 'blur' }
+        ]
       }
     }
   },
@@ -179,7 +221,7 @@ export default {
       this.queryInfo.pagenum = newCurrent
       this.getUserList()
     },
-    // [添加用户]对话框关闭
+    // [添加用户]对话框关闭处理函数
     addDialogClose () {
       // 重置[添加用户]表单
       this.$refs.addFormRef.resetFields()
@@ -203,6 +245,41 @@ export default {
         // 成功
         this.$message.success('添加用户成功！')
         this.addDialogVisible = false
+        this.getUserList()
+      })
+    },
+    // [添加用户]对话框关闭处理函数
+    editDialogClose () {
+      this.$refs.editFormRef.resetFields()
+      this.editForm = {}
+    },
+    // [编辑用户]对话框显示
+    async showEditDialog (id) {
+      const { data: res } = await this.$http.get('users/' + id)
+      if (res.meta.status !== 200) {
+        this.$message.error('获取用户信息失败！')
+        return
+      }
+      this.editForm = res.data
+      this.editDialogVisible = true
+    },
+    // [编辑用户]表单的提交按钮
+    updateUser () {
+      // 提交前预验证
+      this.$refs.editFormRef.validate(async valid => {
+        // 验证失败
+        if (!valid) {
+          return
+        }
+        // 验证成功，开始提交
+        const { data: res } = await this.$http.put('users/' + this.editForm.id, { email: this.editForm.email, mobile: this.editForm.mobile })
+        if (res.meta.status !== 200) {
+          this.$message.error('更新用户失败')
+          return
+        }
+        this.$message.success('更新用户成功')
+        this.editDialogVisible = false
+        this.editForm = {}
         this.getUserList()
       })
     }
