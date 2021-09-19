c9c0618d7281ab73d0712d59e2cd75014fd5f489
GAN2021 <gan2021@com.com>
2021-09-19 12:20:42 +0800

    分类级联选择器完成

--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -14,7 +14,14 @@
       <el-row class="cat-opt">
         <el-col>
           选择商品分类：
-          <el-cascader></el-cascader>
+          <el-cascader
+            expand-trigger="hover"
+            :options="cateList"
+            :props="cateProps"
+            v-model="seletedKeys"
+            clearable
+            @change="handleCateChange"
+          ></el-cascader>
         </el-col>
       </el-row>
     </el-card>
@@ -24,15 +31,48 @@
 <script>
 export default {
   data () {
-    return {}
+    return {
+      // 所有分类列表
+      cateList: [],
+      // 分类级联选择器配置项
+      cateProps: {
+        label: 'cat_name',
+        value: 'cat_id',
+        children: 'children'
+      },
+      // 分类级联选择器选中项
+      seletedKeys: []
+    }
   },
-  methods: {},
-  created () { }
+  methods: {
+    // 获取所有分类
+    async getCateList () {
+      const { data: res } = await this.$http.get('categories')
+      if (res.meta.status !== 200) {
+        this.$message.error('分类列表获取失败！')
+        return
+      }
+      this.cateList = res.data
+      console.log(res.data)
+    },
+    // 级联选择器选中变化处理函数
+    handleCateChange () {
+      if (this.seletedKeys.length !== 3) {
+        this.seletedKeys = []
+      }
+    }
+  },
+  created () {
+    this.getCateList()
+  }
 }
 </script>
 
 <style lang="less" scoped>
 .cat-opt {
   margin-top: 15px;
+}
+.el-cascader {
+  width: 50%;
 }
 </style>
