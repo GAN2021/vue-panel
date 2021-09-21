f1b3ce8943adee1e91c520f79f24c52bb0ff7d19
GAN2021 <gan2021@com.com>
2021-09-21 15:05:26 +0800

    商品添加之页面布局步骤条

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -6,13 +6,30 @@
       <el-breadcrumb-item>商品管理</el-breadcrumb-item>
       <el-breadcrumb-item :to="{path:'/goods/add'}">商品添加</el-breadcrumb-item>
     </el-breadcrumb>
+
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <el-alert title="添加商品信息" type="info" show-icon center :closable="false"></el-alert>
+      <!-- 步骤条区域 -->
+      <el-steps :active="activeStepIndex" finish-status="success" align-center>
+        <el-step title="基本信息"></el-step>
+        <el-step title="商品参数"></el-step>
+        <el-step title="商品属性"></el-step>
+        <el-step title="商品图片"></el-step>
+        <el-step title="商品内容"></el-step>
+        <el-step title="完成"></el-step>
+      </el-steps>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
   data () {
-    return {}
+    return {
+      // 默认激活步骤索引
+      activeStepIndex: 0
+    }
   },
   methods: {},
   created () { }
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -32,7 +32,9 @@ import {
   Cascader,
   Alert,
   Tabs,
-  TabPane
+  TabPane,
+  Steps,
+  Step
 } from 'element-ui'
 
 Vue.use(Form)
@@ -67,6 +69,8 @@ Vue.use(Cascader)
 Vue.use(Alert)
 Vue.use(Tabs)
 Vue.use(TabPane)
+Vue.use(Steps)
+Vue.use(Step)
 // 全局挂载
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
