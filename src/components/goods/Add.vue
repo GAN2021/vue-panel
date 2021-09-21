1c734d9b7fa271df9f59519a36ae43a2adc27034
GAN2021 <gan2021@com.com>
2021-09-21 15:27:33 +0800

    商品添加之表单包裹tabs

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -10,8 +10,10 @@
     <!-- 卡片视图区域 -->
     <el-card>
       <el-alert title="添加商品信息" type="info" show-icon center :closable="false"></el-alert>
+
       <!-- 步骤条区域 -->
-      <el-steps :active="activeStepIndex" finish-status="success" align-center>
+      <!-- 不转成数字好像也能工作 -->
+      <el-steps :active="parseInt(activeStepIndex)" finish-status="success" align-center>
         <el-step title="基本信息"></el-step>
         <el-step title="商品参数"></el-step>
         <el-step title="商品属性"></el-step>
@@ -19,6 +21,24 @@
         <el-step title="商品内容"></el-step>
         <el-step title="完成"></el-step>
       </el-steps>
+
+      <!-- 添加商品表单 -->
+      <el-form
+        :model="addForm"
+        ref="addFormRef"
+        :rules="addFormRules"
+        label-width="70px"
+        label-position="top"
+      >
+        <!-- tabs区域 -->
+        <el-tabs v-model="activeStepIndex" tab-position="left">
+          <el-tab-pane label="基本信息" name="0">基本信息</el-tab-pane>
+          <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
+          <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
+          <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
+          <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
+        </el-tabs>
+      </el-form>
     </el-card>
   </div>
 </template>
@@ -27,8 +47,13 @@
 export default {
   data () {
     return {
-      // 默认激活步骤索引
-      activeStepIndex: 0
+      // 激活步骤索引、tab名称
+      // 赋值给steps时不转数字看起来也能工作
+      activeStepIndex: '0',
+      // 添加商品的表单数据对象
+      addForm: {},
+      addFormRules: {}
+
     }
   },
   methods: {},
